3c84b232e5ee075a49250f79394a08b6d7dc5d46
xiaoshen <xiaoshen@eridani.club>
2023-02-20 23:16:36 -0600

    add logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0-RC")
 
+    implementation("org.tinylog:tinylog-api-kotlin:2.6.0")
+    implementation("org.tinylog:tinylog-impl:2.6.0")
+
     testImplementation(kotlin("test"))
     testImplementation("io.kotest:kotest-assertions-core:5.5.4")
     testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
